d3ef2131fab397a8666c8eb5e59a203063777e75
MGX DEV <mgx.coding@gmail.com>
2023-04-26 14:59:00 +0200

    Added host bindings and listeners

--- src/app/pages/todos/list-todos/todo-item/todo-item.component.ts
+++ src/app/pages/todos/list-todos/todo-item/todo-item.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
@@ -13,4 +20,19 @@ export class TodoItemComponent {
   // ? Output : send data to the parent element (call "emit")
   @Output()
   done = new EventEmitter<void>();
+
+  // ? Bind to an attribute of the component (style.xxx, class.xxx, attr.xxx, etc)
+  @HostBinding('style.opacity')
+  get componentOpacity() {
+    return this.todo?.done ? 0.3 : 1;
+  }
+
+  // ? Listen to events on the component
+  // ? To get the event, follow this syntax
+  @HostListener('click', ['$event'])
+  onComponentClick(event: MouseEvent) {
+    console.log(
+      `Clicking on the card at position ${event.clientX},${event.clientY} is useless, click on the button !`
+    );
+  }
 }
