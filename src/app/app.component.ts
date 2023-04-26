4a2995e946a454bcd21636f6d7ec8ac019ea2bbe
MGX DEV <mgx.coding@gmail.com>
2023-04-26 11:22:30 +0200

    Added router links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  // ? Dependency injection in Angular : inject what you want in the router
+  constructor(private router: Router) {
+    // ? Other ways of navigating in an application
+    // this.router.navigate(['/']);
+    // this.router.navigateByUrl('/add-todo');
+  }
 }
